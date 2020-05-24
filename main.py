d4526fb97c17ecd47a31bdc08033f324983a25b5
Steven van Dijk <zteven@gmail.com>
2020-05-24 14:01:18 +0200

    CatMeow app

--- main.py
+++ main.py
@@ -5,29 +5,44 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
 
-red = [1,0,0,1]
-green = [0,1,0,1]
-blue =  [0,0,1,1]
-purple = [1,0,1,1]
-
-class HBoxLayoutExample(App):
+class CatMeowApp(App):
+    sound = SoundLoader.load('cat_meow_x.wav')
+    img_still = Image(source='output-onlineimagetools.png', allow_stretch=True)
+    img_playing = Image(source='meow.gif', allow_stretch=True)
+    LengthGifInSeconds = 2
+    layout = BoxLayout(padding=10, orientation='vertical')
+    playing = False
+    
     def build(self):
-        rootLayout = BoxLayout()
-        layout = BoxLayout(padding=10, orientation='vertical')
-        colors = [red, green, blue, purple]
+        btn = Button(text = "Meow?")
+        btn.bind(on_press=self.button_pressed)
 
-        for i in range(5):
-            btn = Button(text=f"Button #{i+1}",
-                         background_color=random.choice(colors)
-                         )
-            layout.add_widget(btn)
-        
-        rootLayout.add_widget(layout)
-        rootLayout.add_widget(Image(source='ramiz-dedakovic-jerh2Nj1XWY-unsplash.jpg'))    
-        
-        return rootLayout
+        if (self.playing):
+            self.layout.add_widget(self.img_playing)
+            Clock.schedule_once(self.set_still_layout, self.LengthGifInSeconds)
+        else:
+            self.layout.add_widget(self.img_still)
+        self.layout.add_widget(btn)
+
+        return self.layout
+
+    def button_pressed(self, instance):
+        if (not self.playing):
+            self.set_layout(True)
+            self.sound.play()
+
+    def set_layout(self, playing):
+        self.layout.clear_widgets()
+        self.playing = playing
+        self.build()
+
+    def set_still_layout(self, dt) :
+        self.set_layout(False)
 
 if __name__ == "__main__":
-    app = HBoxLayoutExample()
+    app = CatMeowApp()
     app.run()
